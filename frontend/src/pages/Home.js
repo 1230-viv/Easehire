cdcb5a139e5f6517243505e94b83648f30fe676a
1230-viv <viveksj1230@gmail.com>
2026-01-31 06:00:31 +0530

    git actions fixes 13

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
       <Navbar />
       <div className="home-container">
         <div className="text-container">
-          <h1>sarthak</h1>
+          <h1>Aditya</h1>
           <p>
             EaseHire streamlines hiring by analyzing resumes, generating ATS scores, 
             and automating candidate screening. Find the best talent faster and make 
